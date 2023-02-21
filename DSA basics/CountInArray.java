b5e903650fc0ef881281be3d7f2b198a1bf708aa
Vishal Singh <vishal.s@zyapaar.com>
2023-02-21 12:04:33 +0530

    zyapaar services Arranged

---
+++ DSA basics/CountInArray.java
@@ -0,0 +1,38 @@
+package com.zyapaar.analytics.properties;
+
+import java.util.*;
+
+public class FindCount {
+	public static void main(String[] args) {
+
+		String str = "this it tips";
+		char[] arr = str.toCharArray();
+			int [] fr = new int [arr.length];  
+			int visited = -1;  
+			for(int i = 0; i < arr.length; i++){  
+					int count = 1;  
+					for(int j = i+1; j < arr.length; j++){  
+							if(arr[i] == arr[j]){  
+									count++;  
+									//To avoid counting same element again  
+									fr[j] = visited;  
+							}  
+					}  
+					if(fr[i] != visited)  
+							fr[i] = count;  
+			}  
+
+			//Displays the frequency of each element present in array  
+			System.out.println("---------------------------------------");  
+			System.out.println(" Element | Frequency");  
+			System.out.println("---------------------------------------");  
+			for(int i = 0; i < fr.length; i++){  
+					if(fr[i] != visited && arr[i] != ' ' )  
+							System.out.println("    " + arr[i] + "    |    " + fr[i]);  
+			}  
+
+	
+		
+		
+  }
+}
